ac5616f9676386ef82195111a5594cde2ce78c70
Gabriel Simoes <gabrielsimoesy@gmail.com>
2022-04-29 09:00:27 -0400

    angular: fundamentos

--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 
 import { PhotoComponent } from './photo/photo.component';
+import { PhotoService } from './photo/photo.service';
 
 @NgModule({
   declarations: [PhotoComponent],
+  providers: [PhotoService],
   exports: [PhotoComponent]
 })
 export class PhotosModule { }
